4915ff853bb8048994dcf3c3b7d41aa06f013ca1
alisher <alisherotaboyev072@gmail.com>
2023-04-08 14:25:57 +0500

    burger menu and mobile menu added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
 "use strict";
 
-const mobileMenu = document.querySelector(".mobileMenu");
+const mobileMenu = document.querySelector(".mobile-menu");
 const burger = document.querySelector(".header-top_burgerMenu");
+const burgerTop = document.querySelector(".header-top_burgerMenu__top");
+const burgerBetween = document.querySelector(".header-top_burgerMenu__between");
+const burgerBottom = document.querySelector(".header-top_burgerMenu__bottom");
 
 burger.addEventListener("click", () => {
     mobileMenu.classList.toggle("open");
+});
+
+burger.addEventListener("click", () => {
+    burgerBetween.classList.toggle("hide");
+});
+
+burger.addEventListener("click", () => {
+    burgerTop.classList.toggle("rotate");
+});
+
+burger.addEventListener("click", () => {
+    burgerBottom.classList.toggle("rotate");
+});
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
 });
