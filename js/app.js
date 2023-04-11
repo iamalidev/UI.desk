ee1e19d082ec67e9b962ba7cbc0cc0cecdd2b181
alisher <alisherotaboyev072@gmail.com>
2023-04-11 17:25:45 +0500

    modal window added

--- js/app.js
+++ js/app.js
@@ -6,24 +6,46 @@ const burgerTop = document.querySelector(".header-top_burgerMenu__top");
 const burgerBetween = document.querySelector(".header-top_burgerMenu__between");
 const burgerBottom = document.querySelector(".header-top_burgerMenu__bottom");
 const bgFon = document.querySelector(".bg-fon");
+const orderBtn = document.querySelector(".header-top_order-button");
+const modal = document.querySelector(".modal");
+const modalClose = document.querySelector(".modal-top_closebtn");
+const mobileMenuOrderBtn = document.querySelector(".mobile-menu_body__btn");
 
 
-burger.addEventListener("click", () => {
+burger.addEventListener("click", (e) => {
+    e.stopPropagation()
     mobileMenu.classList.toggle("open");
-});
-
-burger.addEventListener("click", () => {
     burgerBetween.classList.toggle("hide");
-});
-
-burger.addEventListener("click", () => {
     burgerTop.classList.toggle("rotate");
-});
-
-burger.addEventListener("click", () => {
     burgerBottom.classList.toggle("rotate");
+    bgFon.classList.toggle("show");
+    document.body.addEventListener("click", () => {
+        if (mobileMenu.classList.contains("open")) {
+            mobileMenu.classList.remove("open");
+            burgerBetween.classList.remove("hide");
+            burgerTop.classList.remove("rotate");
+            burgerBottom.classList.remove("rotate");
+            bgFon.classList.remove("show");
+        }
+    })
 });
 
-burger.addEventListener("click", () => {
-    bgFon.classList.toggle("show");
-});
+mobileMenu.addEventListener("click", (e) => {
+    e.stopPropagation()
+})
+
+orderBtn.addEventListener("click", () => {
+    modal.classList.add("open");
+    bgFon.classList.add("show");
+})
+
+mobileMenuOrderBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("open")
+    modal.classList.add("open");
+    bgFon.classList.add("show");
+})
+
+modalClose.addEventListener("click", () => {
+    modal.classList.remove("open");
+    bgFon.classList.remove("show");
+})
