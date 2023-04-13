711491f76bb045b0c18705288922f1b4c488b154
alisher <alisherotaboyev072@gmail.com>
2023-04-13 17:55:58 +0500

    header banner changed

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
 "use strict";
 
-const mobileMenu = document.querySelector(".mobile-menu");
-const burger = document.querySelector(".header-top_burgerMenu");
-const burgerTop = document.querySelector(".header-top_burgerMenu__top");
 const burgerBetween = document.querySelector(".header-top_burgerMenu__between");
 const burgerBottom = document.querySelector(".header-top_burgerMenu__bottom");
-const bgFon = document.querySelector(".bg-fon");
-const orderBtn = document.querySelector(".header-top_order-button");
-const modal = document.querySelector(".modal");
-const modalClose = document.querySelector(".modal-top_closebtn");
 const mobileMenuOrderBtn = document.querySelector(".mobile-menu_body__btn");
+const burgerTop = document.querySelector(".header-top_burgerMenu__top");
+const orderBtn = document.querySelector(".header-top_order-button");
+const modalClose = document.querySelector(".modal-top_closebtn");
+const burger = document.querySelector(".header-top_burgerMenu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const bgFon = document.querySelector(".bg-fon");
+const modal = document.querySelector(".modal");
+const bannerTitle = document.querySelector(".header-banner_texts-title").innerHTML;
+
 
 
 burger.addEventListener("click", (e) => {
@@ -49,3 +51,33 @@ modalClose.addEventListener("click", () => {
     modal.classList.remove("open");
     bgFon.classList.remove("show");
 })
+
+class typing {
+    constructor(option) {
+        this.title = document.querySelector(option.title);
+        this.delay = option.delay;
+        this.interval = option.interval;
+
+        this.text = this.title.innerHTML.trim();
+        this.title.innerHTML = "";
+        setTimeout(() => {
+            this.write();
+        }, this.delay);
+    }
+
+    write(i = 0) {
+        this.title.innerHTML += this.text[i];
+        i++;
+        if (i < this.text.length) {
+            setTimeout(() => {
+                this.write(i);
+            }, this.interval);
+        }
+    }
+}
+
+new typing({
+    title: ".header-banner_texts-text",
+    delay: 100,
+    interval: 20,
+});
